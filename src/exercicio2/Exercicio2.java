3ca5c0ba9f92373228c5cf8e7facdf862eee2d6d
isabela-coder <81197670+isabela-coder@users.noreply.github.com>
2021-03-27 00:10:43 -0300

    Aula 22-03 - Pacotes MVC criados e Formulario CADPESSOA

---
+++ src/exercicio2/Exercicio2.java
@@ -0,0 +1,29 @@
+
+package exercicio2;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+     public static void main(String[] args) {
+         
+         System.out.println("Digite a tabuada que você deseja: ");
+        
+         Scanner in = new Scanner(System.in);
+        
+        int tabuada = in.nextInt();
+
+        for(int i = 10; i >= 1; i--){
+            
+            System.out.println(tabuada + " x " + i +" = " +(tabuada*i));
+                                 
+                               
+                                
+                                 
+         
+         
+     }
+}
+
+}
+
+                                 
